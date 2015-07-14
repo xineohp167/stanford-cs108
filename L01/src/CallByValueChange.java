9cba276a26b47d2123fe3a9c4f967aa6565967a4
fabien <fabien.kunis@gmail.com>
2015-07-14 07:08:45 +0300

    Update the example

--- L01/src/CallByValueChange.java
+++ L01/src/CallByValueChange.java
@@ -2,14 +2,21 @@ import java.awt.Point;
 
 public class CallByValueChange {
 
-	public static void change(Point a) {
-		a = new Point(5,5);
+	public static void notChange(MyPoint a) {
+		a = new MyPoint(5,5);
 	}
 	
 	public static void main(String[] args) {
-		Point p = new Point(1,1);
+		MyPoint p = new MyPoint(1,1);
 		
-		change(p);
+		notChange(p);
 		System.out.println("x=" + p.x + ";y=" + p.y);
+		p = change(p);
+		System.out.println("x=" + p.x + ";y=" + p.y);
+	}
+
+	private static MyPoint change(MyPoint p) {
+		return new MyPoint(5,5);
+		
 	}
 }
