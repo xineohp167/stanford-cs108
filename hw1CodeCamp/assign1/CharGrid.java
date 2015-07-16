c5b81b939f58123c574f601493a60ad5ac4fed43
fabien <fabien.kunis@gmail.com>
2015-07-16 09:45:53 +0300

    Initialize Homework1

---
+++ hw1CodeCamp/assign1/CharGrid.java
@@ -0,0 +1,43 @@
+// HW1 2-d array Problems
+// CharGrid encapsulates a 2-d grid of chars and supports
+// a few operations on the grid.
+
+package assign1;
+
+public class CharGrid {
+	private char[][] grid;
+
+	/**
+	 * Constructs a new CharGrid with the given grid.
+	 * Does not make a copy.
+	 * @param grid
+	 */
+	public CharGrid(char[][] grid) {
+		this.grid = grid;
+	}
+	
+	/**
+	 * Returns the area for the given char in the grid. (see handout).
+	 * @param ch char to look for
+	 * @return area for given char
+	 */
+	public int charArea(char ch) {
+		/**
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid.height){
+
+			}
+		}
+		**/
+		return 0; // TODO ADD YOUR CODE HERE
+	}
+	
+	/**
+	 * Returns the count of '+' figures in the grid (see handout).
+	 * @return number of + in grid
+	 */
+	public int countPlus() {
+		return 0; // TODO ADD YOUR CODE HERE
+	}
+	
+}
---
+++ hw1CodeCamp/assign1/TabooTest.java
@@ -0,0 +1,11 @@
+// TabooTest.java
+// Taboo class tests -- nothing provided.
+package assign1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TabooTest {
+
+	// TODO ADD TESTS
+}
