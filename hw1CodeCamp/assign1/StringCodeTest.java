96bae1fda464136c3b47d99227f208e21e9caa96
fabien <fabien.kunis@gmail.com>
2015-07-17 17:41:51 +0300

    Add test for stringIntersect

--- hw1CodeCamp/assign1/StringCodeTest.java
+++ hw1CodeCamp/assign1/StringCodeTest.java
@@ -17,33 +17,32 @@ public class StringCodeTest {
 		assertEquals("xxaaaabb", StringCode.blowup("xx3abb"));
 		assertEquals("xxxZZZZ", StringCode.blowup("2x3Z"));
 	}
-	
+
 	@Test
 	public void testBlowup2() {
 		// things with digits
-		
+
 		// digit at end
 		assertEquals("axxx", StringCode.blowup("a2x3"));
-		
+
 		// digits next to each other
 		assertEquals("a33111", StringCode.blowup("a231"));
-		
+
 		// try a 0
 		assertEquals("aabb", StringCode.blowup("aa0bb"));
 	}
-	
+
 	@Test
 	public void testBlowup3() {
 		// weird chars, empty string
 		assertEquals("AB&&,- ab", StringCode.blowup("AB&&,- ab"));
 		assertEquals("", StringCode.blowup(""));
-		
+
 		// string with only digits
 		assertEquals("", StringCode.blowup("2"));
 		assertEquals("33", StringCode.blowup("23"));
 	}
-	
-	
+
 	//
 	// maxRun
 	//
@@ -52,14 +51,14 @@ public class StringCodeTest {
 		assertEquals(2, StringCode.maxRun("hoopla"));
 		assertEquals(3, StringCode.maxRun("hoopllla"));
 	}
-	
+
 	@Test
 	public void testRun2() {
 		assertEquals(3, StringCode.maxRun("abbcccddbbbxx"));
 		assertEquals(0, StringCode.maxRun(""));
 		assertEquals(3, StringCode.maxRun("hhhooppoo"));
 	}
-	
+
 	@Test
 	public void testRun3() {
 		// "evolve" technique -- make a series of test cases
@@ -70,6 +69,18 @@ public class StringCodeTest {
 		assertEquals(3, StringCode.maxRun("1112233"));
 	}
 
-	// TODO Need test cases for stringIntersect
-	
+
+	public void testStringIntersectBasic() {
+
+		assertTrue(StringCode.stringIntersect("abc", "abcd", 2));
+		assertFalse(StringCode.stringIntersect("aa", "bb", 1));
+	}
+
+	public void testStringIntersectMore() {
+		// Edge cases
+		assertEquals(false, StringCode.stringIntersect("aa", "bb", 0));
+		assertEquals(false, StringCode.stringIntersect("aab", "aabc", 4));
+		assertEquals(true, StringCode.stringIntersect("112233", "111223344", 2));
+	}
+
 }
