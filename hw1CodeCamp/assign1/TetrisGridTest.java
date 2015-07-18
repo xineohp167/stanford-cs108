33b6aaedfec1838776ae5f15f0af6a130c8b948a
fabien <fabien.kunis@gmail.com>
2015-07-18 12:32:32 +0300

    Add TetrisGridTest

--- hw1CodeCamp/assign1/TetrisGridTest.java
+++ hw1CodeCamp/assign1/TetrisGridTest.java
@@ -29,6 +29,99 @@ public class TetrisGridTest {
 		assertTrue( Arrays.deepEquals(after, tetris.getGrid()) );
 	}
 	
-	// TODO ADD TESTS
+	@Test
+	public void testClearMultipleRows(){
+		boolean[][] before =
+			{
+				{true, true, false, false, true},
+				{false, true, true, false, false}
+			};
+		
+		 boolean[][] after =
+             {
+                 {true, false, false, true, false},
+                 {false, true, false, false, false}
+             };
+		 
+		 boolean[][] wrongAfter =
+             {
+				 {true, true, false, false, true},
+				 {false, true, true, false, false}
+             };
+		 
+		 TetrisGrid tetris = new TetrisGrid(before);
+	     tetris.clearRows();
+
+	     assertTrue(Arrays.deepEquals(after, tetris.getGrid()));
+	     assertFalse(Arrays.deepEquals(wrongAfter, tetris.getGrid()));
+	}
+	
+	@Test
+	public void emptyGridShouldBeEmptyGrid(){
+		boolean[][] before ={};
+		
+		 boolean[][] after = {};
+		 
+		 TetrisGrid tetris = new TetrisGrid(before);
+	     tetris.clearRows();
+
+	     assertTrue(Arrays.deepEquals(after, tetris.getGrid()));
+	}
+	
+	@Test
+	public void verticalGridShouldNotBeChanged(){
+		boolean[][] before ={
+				{true, false, true, true ,false}
+		};
+		
+		 boolean[][] after = {
+				 {true, false, true, true ,false}
+		 };
+		 
+		 TetrisGrid tetris = new TetrisGrid(before);
+	     tetris.clearRows();
+
+	     assertTrue(Arrays.deepEquals(after, tetris.getGrid()));
+	}
+	
+	@Test
+	public void testHorizontalGridShouldBeCleared(){
+		boolean[][] before ={
+				{true},
+				{true},
+				{true}
+		};
+		
+		 boolean[][] after = {
+				 {false},
+				 {false},
+				 {false}
+		 };
+		 
+		 TetrisGrid tetris = new TetrisGrid(before);
+	     tetris.clearRows();
+
+	     assertTrue(Arrays.deepEquals(after, tetris.getGrid()));
+	}
+	
+	@Test
+	public void testHorizontalGridShouldNotBeCleared(){
+		boolean[][] before ={
+				{true},
+				{false},
+				{true}
+		};
+		
+		 boolean[][] after = {
+				 {true},
+				 {false},
+				 {true}
+		 };
+		 
+		 TetrisGrid tetris = new TetrisGrid(before);
+	     tetris.clearRows();
+
+	     assertTrue(Arrays.deepEquals(after, tetris.getGrid()));
+	}
 	
 }
