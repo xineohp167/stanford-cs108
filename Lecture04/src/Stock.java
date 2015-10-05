3799c274fcb4b81b19a734631e6ceacdd7dcdf6a
fabien <fabien.kunis@gmail.com>
2015-10-05 23:27:21 +0300

    Adds Stock.java

--- Lecture04/src/Stock.java
+++ Lecture04/src/Stock.java
@@ -2,43 +2,101 @@
 
 public class Stock implements Comparable<Stock>{
 	
+	// Instance variables
 	private String name;
 	private int grams;
 	private int pricePerGram;
 	private int totalPrice;
 	
+	// Default values
+	private static final int INITIAL_GRAMS = 0;
+	private static final int INITIAL_PRICE_PER_GRAM = 0;
+
+	
+	// Constructors
+	
+	/**
+	 * Constructs a new Stock.<br>
+	 * The totalPice is grams * pricePerGram
+	 * @param name the name of the Stock
+	 * @param grams the weight in grams
+	 * @param pricePerGram price for a gram
+	 */
+	public Stock(String name, int grams, int pricePerGram) {
+		this.name = name;
+		this.grams = grams;
+		this.pricePerGram = pricePerGram;
+		this.totalPrice = grams * pricePerGram;
+	}
+	
+	/**
+	 * Constructs a new Stock with name.<br>
+	 * Initializes the object with default values for 
+	 * grams (grams = 0) and pricePerGramf(0).
+	 * @param name the name of the Stock
+	 */
+	public Stock(String name) {
+		this(name, INITIAL_GRAMS, INITIAL_PRICE_PER_GRAM);	
+	}
 
 
+	/**
+	 * Gets the name of the Stock
+	 * @return name of the stock
+	 */
 	public String getName() {
 		return name;
 	}
 
 
+	/**
+	 * Sets the name of the stock
+	 * @param name the new name ot the stock
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 
+	/**
+	 * Gets the name of the stock
+	 * @return grams of the stock
+	 */
 	public int getGrams() {
 		return grams;
 	}
 
 
+	/**
+	 * Sets the weight in grams
+	 * @param grams the new weight in grams
+	 */
 	public void setGrams(int grams) {
 		this.grams = grams;
 	}
 
 
+	/**
+	 * Gets the price per gram of the stock
+	 * @return the price per gram
+	 */
 	public int getPricePerGram() {
 		return pricePerGram;
 	}
 
 
+	/**
+	 * Sets the new price per gram
+	 * @param pricePerGram new price per gram
+	 */
 	public void setPricePerGram(int pricePerGram) {
 		this.pricePerGram = pricePerGram;
 	}
 
-
+	/**
+	 * Gets the total price of the stock
+	 * @return the total price of the stock
+	 */
 	public int getTotalPrice() {
 		return totalPrice;
 	}
