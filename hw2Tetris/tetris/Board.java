63dfd6b85ec4634002beadf6aff0d9dfd55e6293
fabien <fabien.kunis@gmail.com>
2015-10-22 02:15:11 +0300

    Add Board and BoerdTest

--- hw2Tetris/tetris/Board.java
+++ hw2Tetris/tetris/Board.java
@@ -1,201 +1,313 @@
 // Board.java
 package tetris;
 
+import java.util.Arrays;
+
 /**
- CS108 Tetris Board.
- Represents a Tetris board -- essentially a 2-d grid
- of booleans. Supports tetris pieces and row clearing.
- Has an "undo" feature that allows clients to add and remove pieces efficiently.
- Does not do any drawing or have any idea of pixels. Instead,
- just represents the abstract 2-d board.
-*/
-public class Board	{
+ * CS108 Tetris Board. Represents a Tetris board -- essentially a 2-d grid of
+ * booleans. Supports tetris pieces and row clearing. Has an "undo" feature that
+ * allows clients to add and remove pieces efficiently. Does not do any drawing
+ * or have any idea of pixels. Instead, just represents the abstract 2-d board.
+ */
+public class Board {
 	// Some ivars are stubbed out for you:
 	private int width;
 	private int height;
 	private boolean[][] grid;
 	private boolean DEBUG = true;
 	boolean committed;
-	
-	
+	private int[] heights;
+	private int[] widths;
+	private int maxHeight;
+
+	private static final int INITIAL_HEIGHT = 0;
+	private static final boolean FREE_CELL = false;
+	private static final boolean FILLED_CELL = true;
+
+	private boolean[][] Xgrid;
+	private int[] Xheights;
+	private int[] Xwidths;
+	private int XmaxHeight;
+
 	// Here a few trivial methods are provided:
-	
 	/**
-	 Creates an empty board of the given width and height
-	 measured in blocks.
-	*/
+	 * Creates an empty board of the given width and height measured in blocks.
+	 */
 	public Board(int width, int height) {
 		this.width = width;
 		this.height = height;
 		grid = new boolean[width][height];
 		committed = true;
-		
-		// YOUR CODE HERE
+		heights = new int[width];
+		widths = new int[height];
+		maxHeight = INITIAL_HEIGHT;
+
+		Xgrid = new boolean[width][height];
+		Xheights = new int[width];
+		Xwidths = new int[height];
+
+		commit();
+
 	}
-	
-	
+
 	/**
-	 Returns the width of the board in blocks.
-	*/
+	 * Returns the width of the board in blocks.
+	 */
 	public int getWidth() {
 		return width;
 	}
-	
-	
+
 	/**
-	 Returns the height of the board in blocks.
-	*/
+	 * Returns the height of the board in blocks.
+	 */
 	public int getHeight() {
 		return height;
 	}
-	
-	
+
 	/**
-	 Returns the max column height present in the board.
-	 For an empty board this is 0.
-	*/
-	public int getMaxHeight() {	 
-		return 0; // YOUR CODE HERE
+	 * Returns the max column height present in the board. For an empty board
+	 * this is 0.
+	 */
+	public int getMaxHeight() {
+		return maxHeight;
 	}
-	
-	
+
 	/**
-	 Checks the board for internal consistency -- used
-	 for debugging.
-	*/
+	 * Checks the board for internal consistency -- used for debugging.
+	 */
 	public void sanityCheck() {
 		if (DEBUG) {
-			// YOUR CODE HERE
+			int realMaxHeight = 0;
+			int[] realHeights = new int[width];
+			int[] realWights = new int[height];
+
+			for (int x = 0; x < grid.length; x++)
+				for (int y = 0; y < grid[0].length; y++)
+					if (grid[x][y]) {
+						realWights[y]++;
+
+						if (y >= realHeights[x]) {
+							realHeights[x] = y + 1;
+							if (realHeights[x] > realMaxHeight)
+								realMaxHeight = realHeights[x];
+						}
+					}
+			// System.out.println(this.toString());
+			// System.out.println(Arrays.toString(heights) +
+			// Arrays.toString(realHeights));
+			assert (Arrays.equals(realHeights, heights));
+			assert (Arrays.equals(realWights, widths));
+			assert (realMaxHeight == maxHeight);
+
 		}
 	}
-	
+
 	/**
-	 Given a piece and an x, returns the y
-	 value where the piece would come to rest
-	 if it were dropped straight down at that x.
-	 
-	 <p>
-	 Implementation: use the skirt and the col heights
-	 to compute this fast -- O(skirt length).
-	*/
+	 * Given a piece and an x, returns the y value where the piece would come to
+	 * rest if it were dropped straight down at that x.
+	 * 
+	 * <p>
+	 * Implementation: use the skirt and the col heights to compute this fast --
+	 * O(skirt length).
+	 */
 	public int dropHeight(Piece piece, int x) {
-		return 0; // YOUR CODE HERE
+		int y = 0;
+
+		for (int i = 0; i < piece.getWidth(); i++) {
+			int possibleY = getColumnHeight(x + i) - piece.getSkirt()[i];
+			if (possibleY > 0 && y < possibleY)
+				y = possibleY;
+		}
+
+		return y; // YOUR CODE HERE
 	}
-	
-	
+
 	/**
-	 Returns the height of the given column --
-	 i.e. the y value of the highest block + 1.
-	 The height is 0 if the column contains no blocks.
-	*/
+	 * Returns the height of the given column -- i.e. the y value of the highest
+	 * block + 1. The height is 0 if the column contains no blocks.
+	 */
 	public int getColumnHeight(int x) {
-		return 0; // YOUR CODE HERE
+		return heights[x];
 	}
-	
-	
+
 	/**
-	 Returns the number of filled blocks in
-	 the given row.
-	*/
+	 * Returns the number of filled blocks in the given row.
+	 */
 	public int getRowWidth(int y) {
-		 return 0; // YOUR CODE HERE
+		return widths[y];
 	}
-	
-	
+
 	/**
-	 Returns true if the given block is filled in the board.
-	 Blocks outside of the valid width/height area
-	 always return true.
-	*/
+	 * Returns true if the given block is filled in the board. Blocks outside of
+	 * the valid width/height area always return true.
+	 */
 	public boolean getGrid(int x, int y) {
-		return false; // YOUR CODE HERE
+		return x >= width || y >= height || y < 0 || x < 0 || grid[x][y];
 	}
-	
-	
+
 	public static final int PLACE_OK = 0;
 	public static final int PLACE_ROW_FILLED = 1;
 	public static final int PLACE_OUT_BOUNDS = 2;
 	public static final int PLACE_BAD = 3;
-	
+
 	/**
-	 Attempts to add the body of a piece to the board.
-	 Copies the piece blocks into the board grid.
-	 Returns PLACE_OK for a regular placement, or PLACE_ROW_FILLED
-	 for a regular placement that causes at least one row to be filled.
-	 
-	 <p>Error cases:
-	 A placement may fail in two ways. First, if part of the piece may falls out
-	 of bounds of the board, PLACE_OUT_BOUNDS is returned.
-	 Or the placement may collide with existing blocks in the grid
-	 in which case PLACE_BAD is returned.
-	 In both error cases, the board may be left in an invalid
-	 state. The client can use undo(), to recover the valid, pre-place state.
-	*/
+	 * Attempts to add the body of a piece to the board. Copies the piece blocks
+	 * into the board grid. Returns PLACE_OK for a regular placement, or
+	 * PLACE_ROW_FILLED for a regular placement that causes at least one row to
+	 * be filled.
+	 * 
+	 * <p>
+	 * Error cases: A placement may fail in two ways. First, if part of the
+	 * piece may falls out of bounds of the board, PLACE_OUT_BOUNDS is returned.
+	 * Or the placement may collide with existing blocks in the grid in which
+	 * case PLACE_BAD is returned. In both error cases, the board may be left in
+	 * an invalid state. The client can use undo(), to recover the valid,
+	 * pre-place state.
+	 */
 	public int place(Piece piece, int x, int y) {
 		// flag !committed problem
-		if (!committed) throw new RuntimeException("place commit problem");
-			
+		if (!committed)
+			throw new RuntimeException("place commit problem");
+		committed = false;
+		saveState();
 		int result = PLACE_OK;
-		
-		// YOUR CODE HERE
-		
+
+		for (TPoint point : piece.getBody()) {
+
+			int currX = point.x + x;
+			int currY = point.y + y;
+
+			if (currX >= width || currY >= height || currX < 0 || currY < 0)
+				return PLACE_OUT_BOUNDS;
+
+			if (grid[currX][currY])
+				return PLACE_BAD;
+
+			grid[currX][currY] = FILLED_CELL;
+			if (heights[currX] < currY + 1)
+				heights[currX] = currY + 1;
+
+			widths[currY]++;
+
+			if (heights[currX] > maxHeight)
+				maxHeight = heights[currX];
+
+			if (widths[currY] == width)
+				result = PLACE_ROW_FILLED;
+
+		}
+		// System.out.println("afterPlace: "+Arrays.toString(heights) );
 		return result;
 	}
-	
-	
+
 	/**
-	 Deletes rows that are filled all the way across, moving
-	 things above down. Returns the number of rows cleared.
-	*/
+	 * Deletes rows that are filled all the way across, moving things above
+	 * down. Returns the number of rows cleared.
+	 */
+
 	public int clearRows() {
+		committed = false;
+
 		int rowsCleared = 0;
-		// YOUR CODE HERE
+		int toRow = 0;
+
+		for (int fromRow = 0; fromRow < height; fromRow++) {
+			if (widths[fromRow] == width)
+				rowsCleared++;
+			else if (rowsCleared > 0) {
+				fixRow(toRow, fromRow);
+				toRow++;
+			} else
+				toRow++;
+		}
+
+		while (toRow < height) {
+			widths[toRow] = INITIAL_HEIGHT;
+			for (int i = 0; i < width; i++) {
+				grid[i][toRow] = FREE_CELL;
+				heights[i]--;
+			}
+			maxHeight--;
+			toRow++;
+		}
+
 		sanityCheck();
 		return rowsCleared;
 	}
 
+	private void fixRow(int toRow, int fromRow) {
+		widths[toRow] = widths[fromRow];
+		for (int i = 0; i < width; i++)
+			grid[i][toRow] = grid[i][fromRow];
 
+	}
 
 	/**
-	 Reverts the board to its state before up to one place
-	 and one clearRows();
-	 If the conditions for undo() are not met, such as
-	 calling undo() twice in a row, then the second undo() does nothing.
-	 See the overview docs.
-	*/
+	 * Reverts the board to its state before up to one place and one
+	 * clearRows(); If the conditions for undo() are not met, such as calling
+	 * undo() twice in a row, then the second undo() does nothing. See the
+	 * overview docs.
+	 */
 	public void undo() {
-		// YOUR CODE HERE
+
+		if (!committed) {
+			committed = true;
+
+			boolean[][] temp = grid;
+			grid = Xgrid;
+			Xgrid = temp;
+
+			int[] tempX = heights;
+			heights = Xheights;
+			Xheights = tempX;
+
+			int[] tempY = widths;
+			widths = Xwidths;
+			Xwidths = tempY;
+
+			maxHeight = XmaxHeight;
+		}
 	}
-	
-	
+
 	/**
-	 Puts the board in the committed state.
-	*/
+	 * Puts the board in the committed state.
+	 */
 	public void commit() {
-		committed = true;
+		if (!committed) {
+			committed = true;
+
+		}
 	}
 
+	private void saveState() {
+		for (int i = 0; i < width; i++)
+			System.arraycopy(grid[i], 0, Xgrid[i], 0, height);
 
-	
+		System.arraycopy(heights, 0, Xheights, 0, width);
+		System.arraycopy(widths, 0, Xwidths, 0, height);
+
+		XmaxHeight = maxHeight;
+	}
+
 	/*
-	 Renders the board state as a big String, suitable for printing.
-	 This is the sort of print-obj-state utility that can help see complex
-	 state change over time.
-	 (provided debugging utility) 
+	 * Renders the board state as a big String, suitable for printing. This is
+	 * the sort of print-obj-state utility that can help see complex state
+	 * change over time. (provided debugging utility)
 	 */
 	public String toString() {
 		StringBuilder buff = new StringBuilder();
-		for (int y = height-1; y>=0; y--) {
+		for (int y = height - 1; y >= 0; y--) {
 			buff.append('|');
-			for (int x=0; x<width; x++) {
-				if (getGrid(x,y)) buff.append('+');
-				else buff.append(' ');
+			for (int x = 0; x < width; x++) {
+				if (getGrid(x, y))
+					buff.append('+');
+				else
+					buff.append(' ');
 			}
 			buff.append("|\n");
 		}
-		for (int x=0; x<width+2; x++) buff.append('-');
-		return(buff.toString());
+		for (int x = 0; x < width + 2; x++)
+			buff.append('-');
+		return (buff.toString());
 	}
 }
-
-
--- hw2Tetris/tetris/BoardTest.java
+++ hw2Tetris/tetris/BoardTest.java
@@ -7,28 +7,30 @@ import org.junit.*;
 public class BoardTest {
 	Board b;
 	Piece pyr1, pyr2, pyr3, pyr4, s, sRotated;
+	Piece l;
 
 	// This shows how to build things in setUp() to re-use
 	// across tests.
-	
+
 	// In this case, setUp() makes shapes,
 	// and also a 3X6 board, with pyr placed at the bottom,
 	// ready to be used by tests.
 	@Before
 	public void setUp() throws Exception {
 		b = new Board(3, 6);
-		
+
 		pyr1 = new Piece(Piece.PYRAMID_STR);
 		pyr2 = pyr1.computeNextRotation();
 		pyr3 = pyr2.computeNextRotation();
 		pyr4 = pyr3.computeNextRotation();
-		
+
+		b.place(pyr1, 0, 0);
+
 		s = new Piece(Piece.S1_STR);
 		sRotated = s.computeNextRotation();
-		
-		b.place(pyr1, 0, 0);
+
 	}
-	
+
 	// Check the basic width/height/max after the one placement
 	@Test
 	public void testSample1() {
@@ -39,7 +41,7 @@ public class BoardTest {
 		assertEquals(1, b.getRowWidth(1));
 		assertEquals(0, b.getRowWidth(2));
 	}
-	
+
 	// Place sRotated into the board, then check some measures
 	@Test
 	public void testSample2() {
@@ -51,10 +53,75 @@ public class BoardTest {
 		assertEquals(3, b.getColumnHeight(2));
 		assertEquals(4, b.getMaxHeight());
 	}
-	
-	// Make  more tests, by putting together longer series of 
+
+	@Test
+	public void testClearRows() {
+
+		b.clearRows();
+
+		assertEquals(0, b.getColumnHeight(0));
+		assertEquals(1, b.getColumnHeight(1));
+		assertEquals(1, b.getMaxHeight());
+		assertEquals(1, b.getRowWidth(0));
+		assertEquals(0, b.getRowWidth(1));
+		b.commit();
+		b.place(pyr2, 1, 0);
+
+		b.clearRows();
+
+		Piece[] pieces = Piece.getPieces();
+		l = pieces[Piece.L2];
+		l = l.fastRotation();
+		l = l.fastRotation();
+
+		b.commit();
+		b.place(l, 0, b.dropHeight(l, 0));
+
+		b.clearRows();
+		assertEquals(0, b.getColumnHeight(1));
+		assertEquals(0, b.getMaxHeight());
+		assertEquals(0, b.getRowWidth(0));
+
+	}
+
+	// Make more tests, by putting together longer series of
 	// place, clearRows, undo, place ... checking a few col/row/max
 	// numbers that the board looks right after the operations.
-	
-	
+	@Test
+	public void testUndo() {
+		b.undo();
+		assertEquals(0, b.getColumnHeight(1));
+		assertEquals(0, b.getMaxHeight());
+		assertEquals(0, b.getRowWidth(0));
+
+	}
+
+	@Test
+	public void testUndo2() {
+		b = new Board(3, 6);
+		b.commit();
+		b.place(s, 0, b.dropHeight(s, 0));
+		b.clearRows();
+		b.clearRows();
+		b.clearRows();
+
+		b.commit();
+		b.place(s, 0, b.dropHeight(s, 0));
+		b.clearRows();
+
+		b.commit();
+		b.place(s, 0, b.dropHeight(s, 0));
+		b.clearRows();
+
+		b.commit();
+		System.out.println(b.toString());
+		assertEquals(5, b.getColumnHeight(0));
+		assertEquals(6, b.getColumnHeight(1));
+		assertEquals(6, b.getMaxHeight());
+		assertEquals(2, b.getRowWidth(0));
+		assertEquals(2, b.getRowWidth(1));
+		assertEquals(2, b.getRowWidth(2));
+
+	}
+
 }
