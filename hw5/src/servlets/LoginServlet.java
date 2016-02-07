9112a87462dc77077c78d85649dc90537feda79c
fabien <fabien.kunis@gmail.com>
2016-02-07 21:37:58 +0200

    Add login

---
+++ hw5/src/servlets/LoginServlet.java
@@ -0,0 +1,58 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.AccountManager;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		AccountManager accountManager = (AccountManager) getServletContext()
+				.getAttribute(AccountManager.ATTRIBUTE_NAME);
+
+		if (accountManager.isCorrectPassword(request.getParameter("name"),
+				request.getParameter("password"))) {
+			RequestDispatcher rd = request.getRequestDispatcher("welcome.jsp");
+			rd.forward(request, response);
+		} else {
+			RequestDispatcher rd = request
+					.getRequestDispatcher("login_fail.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+}
